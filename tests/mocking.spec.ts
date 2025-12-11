a6d286373c24591cd0b2e958171ff8daca09fa1f
anandyadav5ster <anandyadav5ster@gmail.com>
2025-12-11 13:15:21 +0530

    typescippt

--- tests/mocking.spec.ts
+++ tests/mocking.spec.ts
@@ -28,6 +28,21 @@ test('Mocking websocket connection',{},async({page})=>{
             ws.send(JSON.stringify({response:json}))
         })
     })
+});
+test('Handle broken liks', async({page}) =>{
+    await page.goto('https://www.example.com');
+    const links = await page.$$eval('a', as=>as.map(a =>a.href))
+    console.log(links.length);
+    for(const link of links){
+        const response = await page.request.get(link);
+        if(response.ok()){
+                   console.log(`✔ Working Link: ${link} - Status: ${response.status()}`);
+
+        } else{
+             console.log(`❌ Broken Link: ${link} - Status: ${response.status()}`);
+        }
+    }
+    
 })
 
 })
