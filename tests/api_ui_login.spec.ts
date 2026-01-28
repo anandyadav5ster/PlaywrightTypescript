55b366a65ff0db84be43f90215f94820fa259515
anandyadav5ster <anandydav5ster@gmail.com>
2026-01-28 12:27:44 +0530

    reporting format

--- tests/api_ui_login.spec.ts
+++ tests/api_ui_login.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, APIRequestContext,request } from '@playwright/test';
+import { test, expect, APIRequestContext,request, chromium } from '@playwright/test';
 
 /**
  * Interface for the Login response or form data
@@ -48,4 +48,69 @@ test.describe('Automation Exercise: API to UI Integration', () => {
     const loggedInText = page.locator('text=Test Exceptions');
     await expect(loggedInText).toBeVisible();
   });
+  test('Capture network request', async({page}) =>{
+    await page.on('request', request => console.log(request.url()));
+  });
+  test('Emulate mobile device', async({ playwright, browser }) =>{
+    const iphone = playwright.devices['iPhone 16'];
+    const context = await browser.newContext({ ...iphone,
+      permissions : ['geolocation'],
+      geolocation : {latitude:37.7749,longitude:-122.4194}
+     });
+     const page = await context.newPage();
+     const userAgent = await page.evaluate(() => navigator.userAgent);
+     console.log(`user agent is ${userAgent}`);
+     await page.goto('https://www.google.com');
+
+  });
+
+  test('Handle javascript code using evaluate', async({page}) =>{
+    await page.evaluate(() => console.log('javascript code'));
+  });
+
+  test('Handle exception', async({page}) => {
+    await page.goto('https://www.google.com');
+    try{
+    await page.click('text=Google');
+  }
+    catch(error:any){
+        console.error('Error message is ', error.message);
+    }
+  });
+  test('Launch developer tool', async({}) =>{
+    const context = await chromium.launch({headless: false, devtools:true});
+    const page = await context.newPage();
+    await page.on('console', msg => console.log(msg.text()));
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('Capture console log', async({page}) =>{
+    await page.on('console', msg => console.log(msg.text()));
+  });
+
+  test('identifying and switching via titles', async ({ context, page }) => {
+  await page.goto('https://example.com');
+  
+  // Open several tabs
+  await context.newPage().then(p => p.goto('https://playwright.dev'));
+  await context.newPage().then(p => p.goto('https://github.com'));
+
+  // Get all open pages in the current browser context
+  const allPages = context.pages();
+
+  // Find a specific page based on its URL or Title
+  let targetPage;
+  for (const p of allPages) {
+    const title = await p.title();
+    if (title.includes('GitHub')) {
+      targetPage = p;
+      break;
+    }
+  }
+
+  if (targetPage) {
+    await targetPage.bringToFront();
+    console.log('Successfully switched focus to:', await targetPage.title());
+  }
+});
 });
